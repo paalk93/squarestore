f3d7431a2a0f825a418b87d1e4bfd7a3938dd39b
Antice <ken.r.e.madsen@gmail.comgmail.com>
2018-05-28 14:24:39 +0200

    Fikset controller link

--- lib/squarestore_web/controllers/bruker_styring_controller.ex
+++ lib/squarestore_web/controllers/bruker_styring_controller.ex
@@ -1,8 +1,7 @@
-defmodule SquarestoreWeb.HomepageController do
+defmodule SquarestoreWeb.BrukerStyringController do
   use SquarestoreWeb, :controller
 
   def index(conn, _params) do
     render conn, "index.html"
   end
 end
- 
